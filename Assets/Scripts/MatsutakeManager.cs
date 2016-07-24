3cb326be5f9419a8a127e642293735e531f1bb55
fujinetyo <fujinetyo@gmail.com>
2016-07-24 23:47:44 +0900

    TimeCounterクラス、GaugeCounterクラス作成

--- Assets/Scripts/MatsutakeManager.cs
+++ Assets/Scripts/MatsutakeManager.cs
@@ -3,40 +3,60 @@ using System.Collections;
 
 public class MatsutakeManager : MonoBehaviour {
 
-    bool freeMoveFlg = true;
+    bool freeMoveFlg = false;
     Vector3 screenPoint;
 
-	// Use this for initialization
-	void Start () {
+    readonly float movableMaxY = Screen.height / 2.0f;
+    readonly float movableMinY = Screen.height / 4.0f;
+
+    // Use this for initialization
+    void Start () {
         StartCoroutine("MouseTest");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        /*
-        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
-        Debug.Log(screenPoint);
-
-        if (freeMoveFlg)
-        {
-            // 自由にきのこを動かすことができる
-            Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-            transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
-        }
-        else
-        {
-            // 上下にのみきのこを動かすことができる
-        }
-        */
 	}
 
     IEnumerator MouseTest()
     {
         while (true)
         {
+            // ???
             screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+            // ???
             Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
-            transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+
+            // マウスの移動量を取得
+            float sensitivity = 0.1f;
+            float mouse_move_x = Input.GetAxis("Mouse X") * sensitivity;
+            float mouse_move_y = Input.GetAxis("Mouse Y") * sensitivity;
+            //Debug.Log(mouse_move_x + " : " + mouse_move_y);
+
+            if (freeMoveFlg)
+            {
+                // マウスに追従するようにきのこを動かせる
+                transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+            }
+            else
+            {
+                // 上下にのみきのこを動かせる(動ける範囲に制限あり)
+                // X軸を中央（口のある座標）に固定
+                mousePosition = new Vector3(Screen.width / 2.0f, mousePosition.y, mousePosition.z);
+
+                // 上下の上限
+                if (mousePosition.y >= movableMaxY)
+                {
+                    mousePosition = new Vector3(mousePosition.x, movableMaxY, mousePosition.z);
+                }
+                else if (mousePosition.y <= movableMinY)
+                {
+                    mousePosition = new Vector3(mousePosition.x, movableMinY, mousePosition.z);
+                }
+
+                transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+            }
 
             yield return null;
         }
---
+++ Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeCounter : MonoBehaviour {
+
+    float timeCount = 30.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCount -= Time.deltaTime;
+        if (timeCount <= 0.0f)
+        {
+            // 終了
+        }
+
+        Debug.Log(timeCount);
+	}
+}
