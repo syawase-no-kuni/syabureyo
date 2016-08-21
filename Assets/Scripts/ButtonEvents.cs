287ba937ebe7e7715a8289368e011756775e5bca
fujinetyo <fujinetyo@gmail.com>
2016-08-22 00:48:49 +0900

    クリア画面仮実装。ツイッターにつぶやく機能

---
+++ Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using SWorker;
+
+
+public class ButtonEvents : MonoBehaviour {
+    public void retryButton()
+    {
+        Debug.Log("retry");
+        Application.LoadLevel("Main");
+    }
+
+    public void TweetButton()
+    {
+        Debug.Log("tweet");
+
+#if UNITY_IPHONE || UNITY_ANDROID
+        string message = "message";
+        string url = "http://siawase-kingdom.co.jp/";
+        SocialWorker.PostTwitter(message, "");
+#else
+        Application.OpenURL("http://twitter.com/intent/tweet?text="
+            + WWW.EscapeURL("テキスト #hashtag"));
+#endif
+
+    }
+}
